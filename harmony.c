458174f92b59fffda0a5407564b6e8f08825be9e
David <rynstheoverlord@gmail.com>
2025-12-06 13:05:20 +0100

    Following new wlroots api

--- harmony.c
+++ harmony.c
@@ -1,88 +1,81 @@
 #include <assert.h>
-#include <bits/time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <wayland-server-core.h>
-#include <wayland-util.h>
 #include <wlr/backend.h>
+#include <wlr/render/allocator.h>
+#include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_cursor.h>
+#include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_input_device.h>
+#include <wlr/types/wlr_keyboard.h>
 #include <wlr/types/wlr_output.h>
 #include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_pointer.h>
+#include <wlr/types/wlr_scene.h>
+#include <wlr/types/wlr_seat.h>
+#include <wlr/types/wlr_subcompositor.h>
+#include <wlr/types/wlr_xcursor_manager.h>
+#include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/log.h>
+#include <xkbcommon/xkbcommon.h>
 
 struct harmony_server {
-    struct wl_display *wl_display;
-    struct wl_event_loop *wl_event_loop;
-
-    struct wlr_backend *backend;
-    struct wl_listener new_output;
+    struct wlr_backend *backend; 
+    struct wl_display *display;
+    struct wlr_renderer *renderer; 
+    struct wlr_allocator *allocator;
+    struct wl_event_loop *event_loop;
     struct wl_list outputs;
 };
 
-struct harmony_output {
-    struct wlr_output *wlr_output; 
-    struct harmony_server *server; 
-    struct timespec last_frame;
-    struct wl_listener frame;
-    struct wl_list link;
-
-    struct wl_listener destroy;
-};
-
-static void output_destroy_notify(struct wl_listener *listener, void *data) 
-{
-    struct harmony_output *output = wl_container_of(listener, output, destroy);
-    wl_list_remove(&output->link);
-    wl_list_remove(&output->destroy.link);
-    wl_list_remove(&output->frame.link);
-
-    free(output);
-}
-
-static void new_output_notify(struct wl_listener *listener, void *data)
-{
-    struct harmony_server *server = wl_container_of(listener, server, new_output);
-    struct wlr_output *wlr_output = data;
-
-    if (!wl_list_empty(&wlr_output->modes))
-    {
-        struct wlr_output_state state;
-        wlr_output_state_init(&state);
-
-        struct wlr_output_mode *mode = wl_container_of(wlr_output->modes.prev, mode, link);
-        wlr_output_state_set_custom_mode(&state, mode->width, mode->height, mode->refresh);
-    }
-
-    struct harmony_output *output = calloc(1, sizeof(struct harmony_output));
-    clock_gettime(CLOCK_MONOTONIC, &output->last_frame);
-    output->server = server; 
-    wl_list_insert(&server->outputs, &output->link);
-
-    output->destroy.notify = output_destroy_notify;
-    wl_signal_add(&wlr_output->events.destroy, &output->destroy);
-}
 
 int main(void) 
 {
     struct harmony_server server; 
-    server.wl_display = wl_display_create();
-    assert(server.wl_display);
 
-    server.wl_event_loop = wl_display_get_event_loop(server.wl_display);
-    assert(server.wl_event_loop);
+    server.display = wl_display_create();
+    assert(server.display); 
 
-    server.backend = wlr_backend_autocreate(server.wl_event_loop, NULL);
-    assert(server.backend);
+   server.backend = wlr_backend_autocreate(wl_display_get_event_loop(server.display), NULL);
+     if (server.backend == NULL) {
+          wlr_log(WLR_ERROR, "failed to create wlr_backend");
+          return 1;
+      }
 
-    wl_list_init(&server.outputs);
-    server.new_output.notify = new_output_notify;
+   server.renderer = wlr_renderer_autocreate(server.backend);
+	if (server.renderer == NULL) {
+		wlr_log(WLR_ERROR, "failed to create wlr_renderer");
+		return 1;
+	}
+
+    wlr_renderer_init_wl_display(server.renderer, server.display);
+
+    server.allocator = wlr_allocator_autocreate(server.backend, server.renderer);
+	if (server.allocator == NULL) {
+		wlr_log(WLR_ERROR, "failed to create wlr_allocator");
+		return 1;
+	}
+
+    server.event_loop = wl_display_get_event_loop(server.display);
+    assert(server.event_loop);
 
     if (!wlr_backend_start(server.backend))
     {
         fprintf(stderr, "Failed to start backend!\n");
-        wl_display_destroy(server.wl_display);
+        wl_display_destroy(server.display);
         return 1;
     }
-    wl_display_run(server.wl_display);
-    wl_display_destroy(server.wl_display);
+
+
+    // wlr_compositor_create(server.display, 5, server.renderer);
+    // wlr_subcompositor_create(server.display);
+
+    printf("Starting Harmony...");
+    wl_display_run(server.display);
+    wl_display_destroy(server.display);
+
     return 0;
 }
