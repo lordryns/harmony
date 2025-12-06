823112ef7be042cf4d27a94dc2de29a0068394a6
David <rynstheoverlord@gmail.com>
2025-12-06 10:31:12 +0100

    Setting up compositor foundation

--- harmony.c
+++ harmony.c
@@ -1,15 +1,51 @@
 #include <assert.h>
+#include <bits/time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <wayland-server-core.h>
+#include <wayland-util.h>
 #include <wlr/backend.h>
+#include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_output_layout.h>
 
 struct harmony_server {
     struct wl_display *wl_display;
     struct wl_event_loop *wl_event_loop;
 
     struct wlr_backend *backend;
+    struct wl_listener new_output;
+    struct wl_list outputs;
 };
 
+struct harmony_output {
+    struct wlr_output *wlr_output; 
+    struct harmony_server *server; 
+    struct timespec last_frame;
+    struct wl_list link;
+
+    struct wl_listener destroy;
+};
+
+static void new_output_notify(struct wl_listener *listener, void *data)
+{
+    struct harmony_server *server = wl_container_of(listener, server, new_output);
+    struct wlr_output *wlr_output = data;
+
+    if (!wl_list_empty(&wlr_output->modes))
+    {
+        struct wlr_output_state state;
+        wlr_output_state_init(&state);
+
+        struct wlr_output_mode *mode = wl_container_of(wlr_output->modes.prev, mode, link);
+        wlr_output_state_set_custom_mode(&state, mode->width, mode->height, mode->refresh);
+    }
+
+    struct harmony_output *output = calloc(1, sizeof(struct harmony_output));
+    clock_gettime(CLOCK_MONOTONIC, &output->last_frame);
+    output->server = server; 
+    wl_list_insert(&server->outputs, &output->link);
+}
 
 int main(void) 
 {
@@ -23,9 +59,8 @@ int main(void)
     server.backend = wlr_backend_autocreate(server.wl_event_loop, NULL);
     assert(server.backend);
 
-	if (server.backend == NULL) { 
-		return 1;
-	}
+    wl_list_init(&server.outputs);
+    server.new_output.notify = new_output_notify;
 
     if (!wlr_backend_start(server.backend))
     {
